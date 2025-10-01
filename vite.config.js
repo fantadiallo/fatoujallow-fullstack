1fd9ab27c0d47b0cf430ca1da73d7c058476f6c5
fantadiallo <fatmbo0003@outlook.com>
2025-10-01 15:24:50 +0200

    chore: update HTML head and Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "src") }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // NOTE: no .scss extension; Sass will resolve _variables.scss
+        additionalData: '@use "@/styles/variables" as *;\n'
+      }
+    }
+  }
+});
