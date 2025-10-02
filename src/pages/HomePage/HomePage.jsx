f130d5b545925639efb03302c1d22b2ec3398c31
fatou Jallow <128003934+fantadiallo@users.noreply.github.com>
2025-10-02 13:09:06 +0200

    fix:

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import About from "../../components/About/About.jsx";
 import Hero from "../../components/Hero/Hero.jsx";
 import Skills from "../../components/skills/Skills.jsx";
-import Resume from "../../components/Resume/Resume.jsx";
+import Resume from "../../components/resume/Resume.jsx";
 
 /**
  * HomePage component representing the main landing page of the portfolio.
